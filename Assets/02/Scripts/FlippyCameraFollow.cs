1d080c90af935f82159f274ca3cb096a222d065a
Jason Viers <bean@beanalby.net>
2014-02-16 20:24:59 -0500

    Added basic mechanics

---
+++ Assets/02/Scripts/FlippyCameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlippyCameraFollow : MonoBehaviour {
+    public GameObject target;
+
+    public void Update() {
+        Vector3 pos = transform.position;
+        pos.x = target.transform.position.x;
+        transform.position = pos;
+    }
+}
---
+++ Assets/02/Scripts/FlippyPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlippyPlayer : MonoBehaviour {
+
+    float xSpeed = 3;
+    private float jumpSpeed = 6f;
+
+    // Use this for initialization
+    void Start () {
+        rigidbody2D.velocity = new Vector2(xSpeed, 0);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetButtonDown("Jump")) {
+            rigidbody2D.velocity = new Vector2(xSpeed, jumpSpeed);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D col) {
+        Debug.Log("Player hit " + col.collider.name);
+    }
+}
