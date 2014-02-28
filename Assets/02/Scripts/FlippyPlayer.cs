39b737aa2c3f5a305d48c16fa475ce8898be6225
Jason Viers <bean@beanalby.net>
2014-02-27 23:24:34 -0500

    Added flippy to flippy ball

--- Assets/02/Scripts/FlippyPlayer.cs
+++ Assets/02/Scripts/FlippyPlayer.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class FlippyPlayer : MonoBehaviour {
 
+    private const float VELOCITY_MAX_ANGLE = 8.5f;
+
     public AudioClip jumpSound;
 
     float xSpeed = 3;
@@ -13,6 +15,7 @@ public class FlippyPlayer : MonoBehaviour {
     void Start () {
         rigidbody2D.gravityScale = gravity;
         rigidbody2D.velocity = new Vector2(xSpeed, 0);
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, 45));
     }
     
     // Update is called once per frame
@@ -21,6 +24,11 @@ public class FlippyPlayer : MonoBehaviour {
             AudioSource.PlayClipAtPoint(jumpSound, transform.position);
             rigidbody2D.velocity = new Vector2(xSpeed, jumpSpeed);
         }
+        // adjust the rotation based on velocity
+        float v = rigidbody2D.velocity.y;
+        v += VELOCITY_MAX_ANGLE;
+        float angle = Mathf.Lerp(360, 0, v / (VELOCITY_MAX_ANGLE * 2));
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 
     public void OnCollisionEnter2D(Collision2D col) {
