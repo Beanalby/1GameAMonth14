e06c6a2fd0302ab76c2f5591a6e3b58a8e93731d
Jason Viers <bean@beanalby.net>
2014-03-24 23:42:25 -0400

    Added 03, first pass at swinging mechanic

---
+++ Assets/03/Scripts/JointGizmo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(DistanceJoint2D))]
+public class JointGizmo : MonoBehaviour {
+
+    public void OnDrawGizmos() {
+        DistanceJoint2D joint = GetComponent<DistanceJoint2D>();
+        Vector3 src = transform.position
+            + new Vector3(joint.anchor.x, joint.anchor.y, 0);
+        Vector3 dest = joint.connectedBody.transform.position
+            + new Vector3(joint.connectedAnchor.x, joint.connectedAnchor.y, 0);
+        //Debug.Log((dest - src).magnitude.ToString(".000000"));
+        if (joint.distance*.99f <= (dest - src).magnitude) {
+            Gizmos.color = Color.red;
+        } else {
+            Gizmos.color = Color.green;
+        }
+        Gizmos.DrawLine(src, dest);
+    }
+}
---
+++ Assets/03/Scripts/SwayPlayer.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwayPlayer : MonoBehaviour {
+
+    private float swingAccel = .5f;
+    private float minAccel = .05f;
+    private float maxVelocity = 5f;
+
+    private float minLen = 1.5f;
+    private float maxLen = 5f;
+    private float lenSpeed = 1f;
+    private float lenThreshold = .5f;
+
+    private DistanceJoint2D joint;
+
+    public void Start() {
+        joint = GetComponent<DistanceJoint2D>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.X)) {
+            transform.position = new Vector3(0, transform.position.y, 0);
+            rigidbody2D.velocity = new Vector2(.1f, 0);
+            joint.distance = 2f;
+        }
+        if(Input.GetKeyDown(KeyCode.C)) {
+            joint.distance = 3f;
+        }
+        if(Input.GetKeyDown(KeyCode.V)) {
+            joint.distance = 4f;
+        }
+        HandleSwing();
+        HandleLengthen();
+    }
+    
+    private void HandleLengthen() {
+        Vector3 us = transform.position
+            + new Vector3(joint.anchor.x, joint.anchor.y, 0);
+        Vector3 jointBase = joint.connectedBody.transform.position
+            + new Vector3(joint.connectedAnchor.x, joint.connectedAnchor.y, 0);
+        // don't allow adjusting length if it's not taunt
+        if (joint.distance * .99f >= (us - jointBase).magnitude) {
+            return;
+        }
+
+        bool moved = false;
+        if (Input.GetAxis("Vertical") > lenThreshold) {
+            joint.distance = Mathf.Max(minLen, joint.distance - lenSpeed * Time.deltaTime);
+            moved = true;
+        } else if (Input.GetAxis("Vertical") < -lenThreshold) {
+            joint.distance = Mathf.Min(maxLen, joint.distance + lenSpeed * Time.deltaTime);
+            moved = true;
+        }
+        if (moved) {
+            // automatically adjust our position for the new length
+            Ray ray = new Ray(jointBase, us - jointBase);
+            transform.position = ray.GetPoint(joint.distance);
+        }
+    }
+
+    private void HandleSwing() {
+        // Don't let input affect the rope's speed unless it's taunt
+        //Vector3 src = transform.position
+        //    + new Vector3(joint.anchor.x, joint.anchor.y, 0);
+        //Vector3 dest = joint.connectedBody.transform.position
+        //    + new Vector3(joint.connectedAnchor.x, joint.connectedAnchor.y, 0);
+
+        //if (joint.distance * .99f >= (dest - src).magnitude) {
+        //    return;
+        //}
+        Vector2 delta = (swingAccel * Time.deltaTime) * rigidbody2D.velocity;
+        Vector2 before = delta;
+        if (delta.magnitude == 0) {
+            delta = Vector2.right * minAccel;
+        } else if (delta.magnitude < minAccel) {
+            delta *= (minAccel / delta.magnitude);
+        }
+        if (Input.GetAxisRaw("Horizontal") == 1) {
+            if (rigidbody2D.velocity.x < 0) {
+                rigidbody2D.velocity -= delta;
+            } else {
+                if(rigidbody2D.velocity.magnitude < maxVelocity) {
+                    rigidbody2D.velocity += delta;
+                }
+            }
+        }
+        if (Input.GetAxisRaw("Horizontal") == -1) {
+            if (rigidbody2D.velocity.x > 0) {
+                rigidbody2D.velocity -= delta;
+            } else {
+                if (rigidbody2D.velocity.magnitude < maxVelocity) {
+                    rigidbody2D.velocity += delta;
+                }
+            }
+        }
+	}
+}
