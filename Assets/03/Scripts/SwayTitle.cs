bfc36619486142278c99ab596454a8177822521d
Jason Viers <bean@beanalby.net>
2014-03-31 22:34:24 -0400

    Added title screen

---
+++ Assets/03/Scripts/SwayTitle.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwayTitle : MonoBehaviour {
+
+    public void Update() {
+        if(Input.GetButtonDown("Jump")) {
+            Application.LoadLevel("stage1");
+        }
+    }
+}
