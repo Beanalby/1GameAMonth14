87c2e24cdc03c0b7fc0348dee494f413d198b2cf
Jason Viers <bean@beanalby.net>
2014-04-26 00:20:54 -0400

    First pass of depth mask for game #4

---
+++ Assets/04/Scripts/MoveMask.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveMask : MonoBehaviour {
+    private float spinSpeed = -100;
+    private float moveSpeed = -5f;
+
+    private bool moving = false;
+
+
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            moving = true;
+        }
+        if (moving) {
+            Vector3 rot = transform.localRotation.eulerAngles;
+            rot.z += spinSpeed * Time.deltaTime;
+            transform.localRotation = Quaternion.Euler(rot);
+
+            Vector3 pos = transform.localPosition;
+            pos.x += moveSpeed * Time.deltaTime;
+            transform.localPosition = pos;
+        }
+    }
+}
