49cf2966dd5103494a1367a7cb67138d0ef1dcbb
Jason Viers <bean@beanalby.net>
2014-05-27 22:57:41 -0400

    Fixed maxAngularVelocity on the ball rigidbody

--- Assets/05/Scripts/SinkBall.cs
+++ Assets/05/Scripts/SinkBall.cs
@@ -2,8 +2,14 @@
 using System.Collections;
 
 public class SinkBall : MonoBehaviour {
+    float startSpeed = 15f;
+    Rigidbody rb;
+
     public void Start() {
+        rb = GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 1000;
         Physics.gravity *= 10;
-        GetComponent<Rigidbody>().velocity = new Vector3(3, 0, 3);
+        
+        rb.velocity = new Vector3(startSpeed, 0, startSpeed);
     }
 }
