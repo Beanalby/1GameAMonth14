9714e863de4e3a8a06211112680c5538d30b9cf1
Jason Viers <bean@beanalby.net>
2014-05-27 20:26:49 -0400

    Added initial 05 concept, ball goes in hole

---
+++ Assets/05/Scripts/SinkBall.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinkBall : MonoBehaviour {
+    public void Start() {
+        Physics.gravity *= 10;
+        GetComponent<Rigidbody>().velocity = new Vector3(3, 0, 3);
+    }
+}
