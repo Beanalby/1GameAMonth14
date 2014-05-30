87c7b120c811d02424f0d296bafb48b36966a336
Jason Viers <bean@beanalby.net>
2014-05-29 22:30:25 -0400

    Added shooting mechanic

--- Assets/05/Scripts/SinkBall.cs
+++ Assets/05/Scripts/SinkBall.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class SinkBall : MonoBehaviour {
     float startSpeed = 0f;
+    float maxMouseDist = 2;
+    float maxShotPower = 15;
     Rigidbody rb;
 
     public void Start() {
@@ -11,5 +13,32 @@ public class SinkBall : MonoBehaviour {
         Physics.gravity *= 10;
         
         rb.velocity = new Vector3(startSpeed, 0, startSpeed);
+    }
+
+    public void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            ShootBall();
+        }
+    }
+    private void ShootBall() {
+        Vector3 shotInfo = GetShotInfo();
+        rb.velocity = shotInfo * maxShotPower;
+    }
+
+    public Vector3 GetShotInfo() {
+        // Find the cursor's point projected onto the ball's plane
+        Plane ballPlane = new Plane(Vector3.up, transform.position);
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float dist;
+        if (!ballPlane.Raycast(mouseRay, out dist)) {
+            Debug.LogError("Mouse ray doesn't intersect ball plane!");
+            Debug.Break();
+            return Vector3.zero;
+        }
+        Vector3 pos = mouseRay.GetPoint(dist);
+        // note how far away the curor is from the ball
+        float mouseDist = (pos - transform.position).magnitude;
+        float lineDist = Mathf.Min(maxMouseDist, mouseDist) / maxMouseDist;
+        return (transform.position - pos).normalized * lineDist;
     }
 }
--- Assets/05/Scripts/SinkGuide.cs
+++ Assets/05/Scripts/SinkGuide.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class SinkGuide : MonoBehaviour {
     LineRenderer lr;
-    float maxGuideDist = 3;
+    float guideDistance = 3;
     private float scrollSpeed = 2f;
-    Camera cam;
     Material mat;
 
+    SinkBall sb;
+
     public void Start() {
-        cam = Camera.main;
+        sb = GetComponent<SinkBall>();
         lr = GetComponentInChildren<LineRenderer>();
         lr.SetVertexCount(2);
         mat = lr.material;
@@ -20,22 +21,8 @@ public class SinkGuide : MonoBehaviour {
     }
     private void UpdateGuide() {
         lr.SetPosition(0, transform.position);
-
-        // Find the cursor's point projected onto the ball's plane
-        Plane ballPlane = new Plane(Vector3.up, transform.position);
-        Ray mouseRay = cam.ScreenPointToRay(Input.mousePosition);
-        float dist;
-        if (!ballPlane.Raycast(mouseRay, out dist)) {
-            Debug.LogError("Mouse ray doesn't intersect ball plane!");
-            Debug.Break();
-            return;
-        }
-        Vector3 pos = mouseRay.GetPoint(dist);
-        // note how far away the curor is from the ball
-        float mouseDist = (pos - transform.position).magnitude;
-        float lineDist = Mathf.Min(maxGuideDist, mouseDist);
-        Vector3 dir = (pos - transform.position).normalized;
-        lr.SetPosition(1, transform.position - (dir * lineDist));
+        Vector3 shot = sb.GetShotInfo();
+        lr.SetPosition(1, transform.position + (shot * guideDistance));
         mat.SetTextureOffset("_MainTex", new Vector2(-Time.time * scrollSpeed, 0));
     }
 }
