4f89defe498f77c110350581a0eb4808c796ac15
Jason Viers <bean@beanalby.net>
2014-07-27 11:09:49 -0400

    Added RailSwitch

---
+++ Assets/07/Scripts/RailSwitch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace onegam_1407 {
+    public class RailSwitch : MonoBehaviour {
+        public RailSection railLeft;
+        public RailSection railRight;
+
+        public void Start() {
+            railLeft.gameObject.SetActive(false);
+            railRight.gameObject.SetActive(true);
+        }
+
+        public void Update() {
+            if(Input.GetKeyDown(KeyCode.Space)) {
+                railLeft.gameObject.SetActive(!railLeft.gameObject.activeSelf);
+                railRight.gameObject.SetActive(!railRight.gameObject.activeSelf);
+            }
+        }
+    }
+}
