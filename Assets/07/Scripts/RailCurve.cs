90b00cd1192090401cfa4c14597915ccaf2e7dac
Jason Viers <bean@beanalby.net>
2014-07-26 23:06:01 -0400

    Added railCurve

---
+++ Assets/07/Scripts/RailCurve.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace onegam_1407 {
+    public class RailCurve : RailSection {
+
+        public bool curveRight = true;
+
+        private float curveSign = 1;
+        // totalDistance = 1/4 of the circumference of the cirle with
+        // the radius 2 = 2*pi*r / 4 = (2*pi*2) / 4 = pi
+        private float curveDistance = Mathf.PI;
+
+        // Use this for initialization
+        void Start() {
+            if(!curveRight) {
+                curveSign = -1f;
+            }
+            //for(float i = 0; i <= 1; i += .25f) {
+            //    Debug.Log(i + "=" + GetPosition(i * curveDistance));
+            //}
+            //Debug.Log("last=" + GetPosition(curveDistance));
+        }
+
+        // Update is called once per frame
+        void Update() {
+        }
+
+        public override Vector3 GetPosition(float position) {
+            return transform.position + RotateAroundPoint(Vector3.zero,
+                transform.rotation * (2*curveSign*Vector3.right),
+                curveSign * (90 / curveDistance) * position);
+        }
+        public override float GetRemainder(float position) {
+            return Mathf.Max(0, position - curveDistance);
+        }
+        public Vector3 RotateAroundPoint(Vector3 point, Vector3 pivot, float angle) {
+            return Quaternion.Euler(0, angle, 0) * (point - pivot) + pivot;
+        }
+
+        public void OnDrawGizmos() {
+            float sign = (curveRight ? 1 : -1);
+            Vector3[] positions = new Vector3[] {
+                new Vector3(0, 0, 0),
+                new Vector3(sign * .2f, 0, .8f),
+                new Vector3(sign * .6f, 0, 1.4f),
+                new Vector3(sign * 1.2f, 0, 1.8f),
+                new Vector3(sign * 2f, 0, 2f)
+            };
+
+            Gizmos.color = Color.cyan;
+            for(int i = 0; i < positions.Length - 1; i++) {
+                Vector3 pos1 = transform.position + (transform.rotation * positions[i]);
+                Vector3 pos2 = transform.position + (transform.rotation * positions[i+1]);
+                pos1.y += .5f;
+                pos2.y += .5f;
+                Gizmos.DrawLine(pos1, pos2);
+            }
+        }
+    }
+}
--- Assets/07/Scripts/RailStraight.cs
+++ Assets/07/Scripts/RailStraight.cs
@@ -4,12 +4,17 @@ using System.Collections;
 namespace onegam_1407 {
     public class RailStraight : RailSection {
 
-        public Vector3 EndPos;
+        public float length = 1f;
+        private Vector3 EndPos;
+
+        public void Start() {
+            EndPos = new Vector3(0, 0, length);
+        }
         public override Vector3 GetPosition(float position) {
             if(position >= EndPos.magnitude) {
-                return transform.position + EndPos;
+                return transform.position + (transform.rotation * EndPos);
             } else {
-                return Vector3.Lerp(transform.position, transform.position + EndPos,
+                return Vector3.Lerp(transform.position, transform.position + (transform.rotation * EndPos),
                     position / EndPos.magnitude);
             }
         }
@@ -19,7 +24,7 @@ namespace onegam_1407 {
 
         public void OnDrawGizmos() {
             Gizmos.color = Color.cyan;
-            Vector3 pos1 = transform.position, pos2 = transform.position + EndPos;
+            Vector3 pos1 = transform.position, pos2 = transform.position + (transform.rotation * new Vector3(0, 0, length));
             pos1.y += .5f;
             pos2.y += .5f;
             Gizmos.DrawLine(pos1, pos2);
