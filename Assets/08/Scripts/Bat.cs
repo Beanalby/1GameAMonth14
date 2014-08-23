41e4a2c627e9ec81f0a5c44878e9aa040a5047fa
Jason Viers <bean@beanalby.net>
2014-08-23 16:45:49 -0400

    08 started, bat attacks a thing!

---
+++ Assets/08/Scripts/Bat.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace onegam_1408 {
+    [RequireComponent(typeof(Animator))]
+    public class Bat : MonoBehaviour {
+
+        private float attackSpeed = 7f;
+        private Animator anim;
+
+        private GameObject target;
+        private float attackStart = -1f;
+
+        public void Start() {
+            anim = GetComponent<Animator>();
+        }
+
+        public void Update() {
+            UpdateAttack();
+            if(Input.GetKeyDown(KeyCode.Space)) {
+                GameObject obj = GameObject.Find("Sphere");
+                Debug.Log("+++ attacking " + obj);
+                Attack(obj);
+            }
+        }
+
+        public void UpdateAttack() {
+            if(attackStart == -1f) {
+                return;
+            }
+            // move us towards the target
+            Vector3 dir = target.transform.position - transform.position;
+            dir.Normalize();
+            transform.position += dir * attackSpeed * Time.deltaTime;
+        }
+
+        public void Attack(GameObject newTarget) {
+            StartCoroutine(_attack(newTarget));
+        }
+
+        private IEnumerator _attack(GameObject newTarget) {
+            anim.SetTrigger("attackStart");
+            yield return new WaitForSeconds(.4f);
+            target = newTarget;
+            attackStart = Time.time;
+        }
+
+        public void OnTriggerEnter2D(Collider2D other) {
+            Debug.Log("+++ " + name + " hit " + other.name);
+            if(other.gameObject.layer == LayerMask.NameToLayer("Attackable")) {
+                Attack(other.gameObject);
+            } else if(other.gameObject.layer == LayerMask.NameToLayer("Player")) {
+                attackStart = -1;
+                anim.SetTrigger("attackStop");
+                rigidbody2D.MovePosition(transform.position + new Vector3(0, 1, 0));
+            }
+        }
+    }
+}
---
+++ Assets/08/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace onegam_1408 {
+    public class Player : MonoBehaviour {
+        private float moveSpeed = 1f;
+        public void Update() {
+            rigidbody2D.MovePosition(rigidbody2D.position
+                + new Vector2(moveSpeed * Time.deltaTime, 0));
+        }
+    }
+}
