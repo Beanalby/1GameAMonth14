8644a642c983785ddf39a06f0d3c56a25f648c9f
Jason Viers <bean@beanalby.net>
2014-10-31 23:03:34 -0400

    Added cubes

---
+++ Assets/10/Scripts/Cube.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace onegam_1410 {
+    public class Cube : MonoBehaviour {
+
+        public Texture hitTex;
+        public AudioClip hitSound;
+        public GameObject deathEffect;
+
+        private int layerPlayer, layerGround;
+
+        private bool gotHit = false;
+
+        public void Start() {
+            layerPlayer = LayerMask.NameToLayer("Player");
+            layerGround = LayerMask.NameToLayer("Ground");
+        }
+        public void OnCollisionEnter(Collision col) {
+            if(!gotHit && col.gameObject.layer == layerPlayer) {
+                // move us away from the car
+                rigidbody.velocity = 3 * col.relativeVelocity;
+                StartCoroutine(GotHit());
+            } else if(!gotHit && col.gameObject.layer == layerGround) {
+                StartCoroutine(GotHit());
+            }
+        }
+
+        private IEnumerator GotHit() {
+            gotHit = true;
+            if(hitSound) {
+                AudioSource.PlayClipAtPoint(hitSound, Camera.main.transform.position);
+            }
+            GetComponent<MeshRenderer>().material.SetTexture("_MainTex", hitTex);
+            yield return new WaitForSeconds(3);
+            GameObject obj = Instantiate(deathEffect) as GameObject;
+            obj.transform.position = transform.position;
+            Destroy(gameObject);
+       }
+    }
+}
---
+++ Assets/10/Scripts/DestroyOnFinish.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace onegam_1410 {
+    [RequireComponent(typeof(ParticleSystem))]
+    public class DestroyOnFinish : MonoBehaviour {
+
+        public void Start() {
+            Destroy(gameObject, GetComponent<ParticleSystem>().duration);
+        }
+    }
+}
