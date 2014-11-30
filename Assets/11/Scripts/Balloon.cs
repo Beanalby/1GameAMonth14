6d69863e1f68c9d8719a027b114cc16cd729eb7f
Jason Viers <bean@beanalby.net>
2014-11-30 13:10:55 -0500

    Added BalloonLauncher basic functionality

---
+++ Assets/11/Scripts/Balloon.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace onegam_1411 {
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Balloon : MonoBehaviour {
+
+        public void Launch() {
+            rigidbody2D.isKinematic = false;
+        }
+    }
+}
---
+++ Assets/11/Scripts/BalloonLauncher.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace onegam_1411 {
+    public class BalloonLauncher : MonoBehaviour {
+
+        public GameObject balloonPrefab;
+
+        private int numColumns = 5;
+        private int numRows = 4;
+        private float launchDelay = .5f;
+        private float xSpacer = 1.5f;
+        private float ySpacer = 1.5f;
+        private List<Balloon> balloons;
+        private int nextBalloon = 0;
+
+        public void Start() {
+            CreateBalloons();
+            StartCoroutine(StartLaunching());
+        }
+
+        private void CreateBalloons() {
+            balloons = new List<Balloon>(numColumns * numRows);
+            List<int> numInColumn = new List<int>(numColumns);
+            int xOffset = -(int)((numColumns - 1) / 2);
+            for(int i = 0; i < numColumns; i++) {
+                numInColumn.Add(0);
+            }
+            for(int i = 0; i < numColumns * numRows; i++) {
+                // find a new column we haven't used
+                int newColumn;
+                while(true) {
+                    newColumn = Random.Range(0, numColumns);
+                    if(numInColumn[newColumn] < numRows) {
+                        break;
+                    }
+                }
+                Balloon newBalloon = ((GameObject)Instantiate(balloonPrefab)).GetComponent<Balloon>();
+                newBalloon.transform.position = transform.position + new Vector3(xSpacer * (newColumn + xOffset), ySpacer * (numInColumn[newColumn] - (numRows-1)), 0);
+                newBalloon.gameObject.name = " Balloon " + i.ToString("D2");
+                balloons.Add(newBalloon);
+                numInColumn[newColumn]++;
+            }
+        }
+
+        private IEnumerator StartLaunching() {
+            while(nextBalloon != balloons.Count) {
+                yield return new WaitForSeconds(launchDelay);
+                balloons[nextBalloon].Launch();
+                nextBalloon++;
+            }
+       }
+    }
+}
