6f74715fc4be9b95a9e8c7ad55f3247e0d2620cf
Jason Viers <bean@beanalby.net>
2014-11-30 21:37:59 -0500

    Added title screen

---
+++ Assets/11/Scripts/TitleDriver.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace onegam_1411 {
+    public class TitleDriver : MonoBehaviour {
+        public Texture2D titleImage;
+
+        public void Update() {
+            if(Input.GetButtonDown("Jump")) {
+                Application.LoadLevel("11game");
+            }
+        }
+        public void OnGUI() {
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), titleImage);
+        }
+    }
+}
