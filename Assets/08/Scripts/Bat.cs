03f5ccb83595acb12fda494f9816b010cae48cdb
U-Beanolith\ninji <Jason Viers>
2019-03-06 22:49:21 -0500

    Updated to Unity 2018.3, working on swingbot bug

--- Assets/08/Scripts/Bat.cs
+++ Assets/08/Scripts/Bat.cs
@@ -71,15 +71,24 @@ namespace onegam_1408 {
             attackStart = Time.time;
         }
 
+        public void AttackPlayer() {
+            Debug.Log("+++ attacked player!");
+            Player.Instance.GotHit(damage);
+            attackStart = -1;
+            isResetting = true;
+            anim.SetTrigger("attackStop");
+        }
+
+        public void OnCollisionEnter2D(Collision2D col) {
+            Debug.Log("+++ bat collided with " + col.otherCollider.name);
+        }
         public void OnTriggerEnter2D(Collider2D other) {
+            Debug.Log("+++ bat triggered with " + other.name);
             if(other.gameObject.layer == layerAttackable && attackStart==-1f) {
                 Attack(other.gameObject);
                 inRange = other.gameObject;
             } else if(other.gameObject.layer == layerPlayer) {
-                Player.Instance.GotHit(damage);
-                attackStart = -1;
-                isResetting = true;
-                anim.SetTrigger("attackStop");
+                AttackPlayer();
             } else if(other.tag == "Portal") {
                 attackStart = -1;
                 isResetting = true;
--- Assets/08/Scripts/Player.cs
+++ Assets/08/Scripts/Player.cs
@@ -110,9 +110,16 @@ namespace onegam_1408 {
         public void DisableControl() {
             canControl = false;
         }
+        public void OnCollisionEnter2D(Collision2D col) {
+            Debug.Log("+++ Player collided with " + col.otherCollider.name);
+        }
         public void OnTriggerEnter2D(Collider2D other) {
-            if(other.CompareTag("Respawn")) {
+            Debug.Log("+++ Player triggered with " + other.name);
+            if (other.CompareTag("Respawn")) {
                 GameDriver.Instance.SetSpawnPoint(other.gameObject);
+            } else {
+                // it could be the player colliding with bat, let it know
+                //other.SendMessage("AttackPlayer", SendMessageOptions.DontRequireReceiver);
             }
         }
         // OnTriggerExit2D not working 
